3b2feeac5b50b1742065c29db2e88fd29b7fcaff
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-09-23 23:10:27 -0700

    Created Color class (auto commit)

--- Lab 14 Color Class/main.cpp
+++ Lab 14 Color Class/main.cpp
@@ -4,8 +4,29 @@ using namespace std;
 
 // COMSC-210 | Lab 14: Color Class | Erick Pascual-Bautista
 
-int main() {
+class Color {
+    // Private: 
+    int red;
+    int green;
+    int blue;
 
+    public: 
+    Color(int red, int green, int blue) : red(red), green(green), blue(blue) {}
+
+    // Getters that return the associated value
+    int getRed() {return red;}
+    int getGreen() {return green;}
+    int getBlue() {return blue;}
+
+    // Setters that set the associtaed value 
+    void setRed(int red) {this->red = red;}
+    void setGreen(int green) {this->green = green;}
+    void setBlue(int blue) {this->blue = blue;}
+
+};
+
+int main() {
+    Color red = Color(0, 188, 250);
     return 0;
 }
 
