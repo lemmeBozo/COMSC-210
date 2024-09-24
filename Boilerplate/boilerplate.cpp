f8a104af7003aa2548129fed6d5e672d4e11bdf5
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-09-23 23:00:33 -0700

    Auto Commit

---
+++ Boilerplate/boilerplate.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+// COMSC-210 | Lab XXXX: XXXXXXXXX | Erick Pascual-Bautista
+
+int main() {
+
+    return 0;
+}
+
---
+++ Lab 14 Color Class/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+// COMSC-210 | Lab 14: Color Class | Erick Pascual-Bautista
+
+int main() {
+
+    return 0;
+}
+
