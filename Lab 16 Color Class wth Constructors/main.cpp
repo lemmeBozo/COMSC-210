514421f37edcbf2e02e9d57f65bf946a832f2b92
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-10-04 17:33:12 -0700

    I lied now im starting (wrong code)

--- Lab 16 Color Class wth Constructors/main.cpp
+++ Lab 16 Color Class wth Constructors/main.cpp
@@ -1,82 +1,46 @@
 #include <iostream>
-#include <string> // for string
-#include <fstream> // for file input
-#include <vector> // for vectors
 #include <iomanip> // for setw
 
 using namespace std;
 
-// COMSC-210 | Lab 15: Movie Class | Erick Pascual-Bautista
+// COMSC-210 | Lab 14: Color Class | Erick Pascual-Bautista
 
-class Movie {
-    // private: 
-    string screenWriter;
-    string title;
-    int yearReleased;
-    
-    public:
-    // Parameterized Constructor
-    Movie(string screenWriter, string title, int yearReleased) 
-    : screenWriter(screenWriter), title(title), yearReleased(yearReleased) {}
+class Color {
+    // Private: 
+    int red;
+    int green;
+    int blue;
 
-    // Basic Constructor
-    Movie() {
-        this->screenWriter = "";
-        this->title = "";
-        this->yearReleased = 0;
-    }
+    public: 
+    // Parameterized constructor function
+    Color(int red, int green, int blue) : red(red), green(green), blue(blue) {}
 
-    // Getters
-    string getScreenWriter() {return this->screenWriter;}
-    string getTitle() {return this->title;}
-    int getYearReleased() {return this->yearReleased;}
+    // Getters that return the associated value
+    int getRed() {return red;}
+    int getGreen() {return green;}
+    int getBlue() {return blue;}
 
-    // Setters
-    void setScreenWriter(string screenWriter) {this->screenWriter = screenWriter;}
-    void setTitle(string title) {this->title = title;}
-    void setYearReleased() {this->yearReleased = yearReleased;}
+    // Setters that set the associtaed value 
+    void setRed(int red) {this->red = red;}
+    void setGreen(int green) {this->green = green;}
+    void setBlue(int blue) {this->blue = blue;}
 
-    // Print method
+    // Print function (prints out rgb values)
     void print() {
-        cout << setw(20) << "Title" << setw(20) << "Screen Writer" << setw(20) << "Year Released" << endl;
-        cout << setw(20) << this->title << setw(20) << this->screenWriter << setw(20) << this->yearReleased << endl;
+        cout << setw(10) << "Red" << setw(10) << "Green" << setw(10) << "Blue" << endl;
+        cout << setw(10) << red << setw(10) << green << setw(10) << blue << endl;
         cout << endl;
     }
+
 };
 
 int main() {
-    ifstream file;
-    file.open("text.txt");
-    if (!file.good()) {
-        cout << "File not found" << endl;
-    }
-
-    string line;
-    vector<Movie> movies;
-
-    while(getline(file, line)) { // Reads title
-        string title;
-        string screenWriter;
-        int yearReleased;
-
-        title = line;
-
-        if (getline(file,line)) { // checks if new line is read
-            yearReleased = stoi(line); // converts string to integer
-        } else {break;} // exits out of execution if new line cannot be read
-
-        if (getline(file,line)) { // checks if new line is read
-            screenWriter = line;
-        } else {break;} // exits out of execution if new line cannot be read
-        movies.push_back(Movie(screenWriter, title, yearReleased));
-    }
-
-    for (int i = 0; i < movies.size(); i++) {
-        movies[i].print();
-    }
-
-    file.close(); // closes file after reading everything
+    Color red = Color(255, 0, 0);
+    Color green = Color(0, 255, 0);
+    Color blue = Color(0, 0, 255);
+    red.print();
+    green.print();
+    blue.print();
     return 0;
 }
 
-/*just a test why can't I commit my stuff*/
