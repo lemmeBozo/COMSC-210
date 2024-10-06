448f9db77a3aa9cfdbe121294426ff1d1acf1a08
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-10-06 16:42:35 -0700

    Starting Work on Lab 19

---
+++ Lab 19 Abstract and Automate Lab 18/LinkedList.h
@@ -0,0 +1,114 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+// This is my own personal linked list that I created for another class
+// I hope you don't mind that I used this for the assignment
+// also the below may not have followed our coding conventions as again it was done in another class
+
+template <typename T>
+class LinkedList {
+    // private
+    struct Node {
+        T data;
+        Node* nextLink;
+        Node() { // default constructor (added due to error from main.cpp)
+            nextLink = nullptr;
+        }
+        Node(T data) { // parameterized constructor
+            this->data = data;
+            nextLink = nullptr;
+        }
+    };
+
+    Node* head;
+    int currSize;
+
+    public:
+    LinkedList();
+
+    ~LinkedList() {clear();}
+
+    void pushFront(T&); // push element to the front of the list (originally called push)
+
+   void pushBack(T&); // ADDING A NEW METHOD (for the lab since items can either be added to front or back of list)
+
+    void pop(); // remove first element from the list
+    T peek(); // returns the value of the first element
+    bool isEmpty() {return (head == nullptr);}
+    int size() {return currSize;}
+    void clear();
+};
+
+template <typename T>
+LinkedList<T>::LinkedList() {
+    head = nullptr;
+    // more code goes here
+    currSize = 0;
+}
+
+template <typename T>
+void LinkedList<T>::pushFront(T& data) {
+    Node* node = new Node(data);
+    node->nextLink = head;
+    head = node;
+    currSize++;
+
+    /*
+    This was my original implementation 
+    before I learned the easier way
+    
+    if (isEmpty()) { // if list is empty
+        head = new Node(data);
+    }
+    else {
+        Node* node = new Node(data)
+        if (head->nextLink == nullptr){
+            head->nextLink = node;
+        }
+        else {
+            Node* tempNode = head->nextLink;
+            head->nextLink = node;
+            node->nextLink = tempNode;
+        }
+    }
+*/
+}
+
+template <typename T>
+void LinkedList<T>::pushBack(T& data) {
+    Node* node = new Node(data); // creates a new node with the data
+    if(isEmpty()) { // If the list is emetpy
+        head = node;
+    } else {
+        Node* temp = head;
+        while(temp->nextLink != nullptr) { // traverese the linked list until we reach the last element
+            temp = temp->nextLink;
+        }
+        temp->nextLink = node; // once last element is reached we set it equal to node (i.e. will be last element in list)
+    }
+    currSize++;
+}
+
+template <typename T>
+void LinkedList<T>::pop() {
+    if (!isEmpty()) {
+        Node* tempPtr = head;
+        head = head->nextLink;
+        delete tempPtr;
+        currSize--;
+    }
+}
+
+template <typename T>
+T LinkedList<T>::peek() {
+    return head->data;
+}
+
+template <typename T>
+void LinkedList<T>::clear() {
+    while(!isEmpty()) { // while the list isn't empty it will pop the elements i.e. delete them
+        pop();
+    }
+}
+
+#endif
---
+++ Lab 19 Abstract and Automate Lab 18/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string> 
+#include "LinkedList.h" // personal linked list I created
+using namespace std;
+
+// COMSC-210 | Lab 18: Movie Reviews | Erick Pascual-Bautista
+
+struct Review {
+    float rating;
+    string comment;
+    Review() { // Default constructor (added due to issue in code)
+        rating = 0;
+        comment = "";
+    }
+
+    Review(float rating, string comment) // parameterized constructor
+        : rating(rating), comment(comment) {}; 
+};
+
+void addReviews(LinkedList<Review>&); // function prototype for adding reviews
+void outputReviews(LinkedList<Review>&); // function prototype for outputing reviews
+
+int main() {
+
+    LinkedList<Review> list;
+    addReviews(list);
+    outputReviews(list);
+
+    return 0;
+}
+
+void addReviews(LinkedList<Review>& list) { // function declaration for adding reviews
+    bool moreReviews = true;
+
+    while (moreReviews) { // while moreReviews = true;
+        float rating = 0;
+        int choice = 0;
+        string comment;
+
+        cout << "Which linked list method would you like to use?" << endl;
+        cout << "\t[1] New nodes are added to the head of the linked list" << endl;
+        cout << "\t[2] New nodes are added to the tail of the linked list" << endl;
+        cout << "Choice: ";
+
+        cin >> choice; // gets user choice
+
+        cout << endl << "Enter movie review rating 0-5 (floats included): " ;
+        cin >> rating; // gets user rating
+        cin.ignore();
+        cout << "Enter move review comments: ";
+        getline(cin, comment); // gets user comment (note gets the whole line instead of only the first string)
+
+        cout << endl << "Would you like to enter anothe review (0 for no 1 for yes): ";
+        Review review(rating,comment); // Instantiates a review struct
+
+        if (choice == 1) { // push review to the front of the list
+            list.pushFront(review);
+        } else { // push review to the back of the list
+            list.pushBack(review);
+        }
+
+        cin >> moreReviews;
+    }
+}
+
+void outputReviews(LinkedList<Review>& list) {
+    // the way my code works is by poping the elements to see the next one
+    // should I have added an iterator to iterate through my linked list to see what each element is?
+    // instead of just poping them?
+    if (list.isEmpty()) {
+        cout << "No reviews to output" << endl; 
+    }
+    int i = 1;
+    while (!list.isEmpty()) { // while the list is not empty run the below code
+        cout << "Outputting all reviews: " << endl;
+        Review currReview = list.peek();
+        cout << "\tReview #" << i << ": " << currReview.rating << ": " << currReview.comment << endl;
+        list.pop();
+        i++;
+    }
+}
