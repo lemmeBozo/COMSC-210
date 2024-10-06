8e9cb603c130f5e2b59917b0e26d376dbebeeb49
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-10-06 14:39:41 -0700

    Starting Work on Lab

---
+++ Lab 18 Movie Reviews/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string> 
+#include "LinkedList.h" // personal linked list I created
+using namespace std;
+
+// COMSC-210 | Lab 18: Movie Reviews | Erick Pascual-Bautista
+
+int main() {
+
+    LinkedList<string> interstellarReviews;
+
+    return 0;
+}
+
