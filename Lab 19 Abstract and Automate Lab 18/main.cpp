81dbf86d566e074207c0ad1d78535978ed66f703
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-10-06 17:04:14 -0700

    10 Minute auto commit

--- Lab 19 Abstract and Automate Lab 18/main.cpp
+++ Lab 19 Abstract and Automate Lab 18/main.cpp
@@ -9,18 +9,21 @@ using namespace std;
 
 // COMSC-210 | Lab 19: Abstract & Automate Lab 18 | Erick Pascual-Bautista
 
-void generateRandomRating();
+float generateRandomRating();
 
 struct Movie {
     string title;
     string comment;
     float rating;
 
-    Movie() {
+    Movie() { // Default Constructor Initilization
         title = "";
         comment = "";
-        rating = 0;
+        rating = generateRandomRating();
     }
+
+    Movie(string title, string comment) // Parameterized member initilization constructor 
+        : title(title), comment(comment), rating(generateRandomRating()) {};
 
 };
 
@@ -31,9 +34,9 @@ int main() {
     return 0;
 }
 
-void generateRandomRating() {
+float generateRandomRating() {
     random_device rd;
     float randomFloat = static_cast<float>(rd()) / rd.max() * 5.0f; // generates random number between 0-5 
     randomFloat = round(randomFloat * 10.0f) / 10.0f; // rounds to the nearest tenth
-    cout << randomFloat << endl;
+    return randomFloat;
 }
