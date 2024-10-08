87b3f6c39779fd3027a7cb5e1edfd94494d34bf0
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-10-08 13:03:44 -0700

    10 minute auto commit

--- Lab 20 Chair Maker 3000/210-demo-chair-1.cpp
+++ Lab 20 Chair Maker 3000/210-demo-chair-1.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
-#include <cstdlib> // for rand(), srand()
-#include <ctime> // for time()
+#include <iomanip>
 
 using namespace std;
-
-// COMSC-210 | Lab 20: Chair Maker 3000 | Erick Pascual-Bautista
-
 const int SIZE = 3;
-
-int generateRandomInt(int, int);
-float generateRandomFloat(float, float);
 
 class Chair {
 private:
@@ -17,9 +10,6 @@ private:
     double * prices;
 public:
     // constructors
-    // TODO
-    //  change legs such that it either equals 3 or 4 legs
-    // change prices such that they range from 100.00 to 999.99
     Chair() {
         prices = new double[SIZE];
         legs = 0;
@@ -59,15 +49,31 @@ public:
 };
 
 int main() {
+    cout << fixed << setprecision(2);
 
-    return 0;
-}
+    //creating pointer to first chair object
+    Chair *chairPtr = new Chair;
+    chairPtr->setLegs(4);
+    chairPtr->setPrices(121.21, 232.32, 414.14);
+    chairPtr->print();
 
-int generateRandomInt(int min, int max) {
+    //creating dynamic chair object with constructor
+    Chair *livingChair = new Chair(3);
+    livingChair->setPrices(525.25, 434.34, 252.52);
+    livingChair->print();
+    delete livingChair;
+    livingChair = nullptr;
+
+    //creating dynamic array of chair objects
+    Chair *collection = new Chair[SIZE];
+    collection[0].setLegs(4);
+    collection[0].setPrices(441.41, 552.52, 663.63);
+    collection[1].setLegs(4);
+    collection[1].setPrices(484.84, 959.59, 868.68);
+    collection[2].setLegs(4);
+    collection[2].setPrices(626.26, 515.15, 757.57);
+    for (int i = 0; i < SIZE; i++)
+        collection[i].print();
     
-    return 
-}
-
-float generateRandomFloat(float min, float max) {
-
+    return 0;
 }
