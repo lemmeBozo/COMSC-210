8020f4e2e4d018d1d5c1ac9482fe6e9b4f50cb67
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-10-13 17:05:12 -0700

    Starting Work

---
+++ Lab 22 Upgrade the DLL CLass/DoublyLinkedList.h
@@ -0,0 +1,201 @@
+#ifndef DOUBLYLINKEDLIST_H
+#define DOUBLYLINKEDLIST_H
+
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class DoublyLinkedList {
+    struct Node {
+        T data;
+        Node* nextLink;
+        Node* prevLink;
+        Node() { 
+            data = T();
+            nextLink = nullptr;
+            prevLink = nullptr;
+        }
+        Node(T data) { 
+            this->data = data;
+            nextLink = nullptr;
+            prevLink = nullptr;
+        }
+    };
+
+    Node* head;
+    Node* tail;
+    int currSize;
+
+    public:
+    // Default Constructor
+    DoublyLinkedList(); 
+
+    // Push methods
+    void pushFront(const T&); 
+    void pushBack(const T&);
+
+    // Insert method
+    void insertAfter(const T&, int);
+
+    // Deletes methods
+    void deleteNode(const T&);
+    void deleteAt(int);
+
+    // Checks if list is empty
+    bool isEmpty() {return (head == nullptr);}
+
+    // returns the size of the list
+    int size() {return currSize;}
+
+    // Prints methods
+    void print();
+    void printReverse();
+
+    // Destructor
+    ~DoublyLinkedList();
+};
+
+template <typename T>
+DoublyLinkedList<T>::DoublyLinkedList() {
+    head = nullptr;
+    tail = nullptr;
+    currSize = 0;
+}
+
+template <typename T>
+void DoublyLinkedList<T>::pushFront(const T& data) {
+    Node * node = new Node(data);
+    if(isEmpty()) { // if the list is empty
+        head = tail = node; 
+    } else { // if the list is not empty 
+        node->nextLink = head;
+        head->prevLink = node;
+        head = node;
+    }
+    currSize++;
+}
+
+template <typename T>
+void DoublyLinkedList<T>::pushBack(const T& data) {
+    Node* node = new Node(data); 
+    if(isEmpty()) { 
+        head = tail = node;
+    } else {
+        tail->nextLink = node;
+        node->prevLink = tail;
+        tail = node;
+    }
+    currSize++;
+}
+
+// FIX THE BELOW METHOD IF IT DOESN"T WORK
+template <typename T>
+void DoublyLinkedList<T>::insertAfter(const T& data, int position) {
+    if (position < 0) {
+        cout << "Position must be >= 0" << endl;
+        return;
+    }
+
+    Node* node = new Node(data);
+    if (!head) {
+        head = tail = node;
+        return;
+    }
+
+    Node* temp = head;
+    for (int i = 0; i < position && temp; i++) { // traverse the list until it reaches the desired position
+        temp = temp->nextLink;
+    }
+
+    if (!temp) { // if node is out of range (caused from the above) then deletes node and returns
+        cout << "Position exceeds list size. Node not inserted." << endl;
+        delete node;
+        return;
+    }
+
+    // otherwise perform the following
+    node->nextLink = temp->nextLink;
+    node->prevLink = temp;
+    if (temp->nextLink) {
+        temp->nextLink->prevLink = node;
+    } else {
+        tail = node;
+    }
+    temp->nextLink = node;
+    currSize++;
+}
+
+template <typename T>
+void DoublyLinkedList<T>::deleteNode(const T& data) {
+    if(isEmpty()) {return;} // if array is empty returns (can't delete everything)
+
+    Node* temp = head;
+    while (temp && temp->data != data) {
+        temp = temp->nextLink;
+    }
+
+    if (!temp) {return;} // value not found
+
+    // Adjusts the previous node's next link
+    if (temp->prevLink) {
+        temp->prevLink->nextLink = temp->nextLink;
+    } else {
+        head = temp->nextLink;
+    }
+
+    // Adjusts the next node's previous link
+    if (temp->nextLink) {
+        temp->nextLink->prevLink = temp->prevLink;
+    } else {
+        tail = temp->prevLink;
+    }
+    delete temp;
+    currSize--;
+}
+
+template <typename T>
+void DoublyLinkedList<T>::deleteAt(int position) {
+
+};
+
+
+
+
+
+
+
+// MAKE SURE TO OVERLOAD << for the GOAT CLASS
+template <typename T>
+void DoublyLinkedList<T>::print() {
+    Node* current = head;
+    if (!current) {return;}
+    while (current) {
+        cout << current->data << " ";
+        current = current->nextLink;
+    }
+    cout << endl;
+}
+
+template <typename T>
+void DoublyLinkedList<T>::printReverse() {
+    Node* current = tail;
+    if (!current) {return;}
+    while (current) {
+        cout << current->data << " ";
+        current = current->prevLink;
+    }
+    cout << endl;
+}
+
+
+template <typename T>
+DoublyLinkedList<T>::~DoublyLinkedList() {
+    while(head) {
+        Node* temp = head;
+        head = head->nextLink;
+        delete temp;
+    }
+}
+
+#endif
---
+++ Lab 22 Upgrade the DLL CLass/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "DoublyLinkedList.h"
+using namespace std;
+
+// COMSC-210 | Lab 21: Goat Herd Manager 3000 | Erick Pascual-Bautista
+
+int main() {
+    DoublyLinkedList list;
+    int size = rand() % (MAX_LS-MIN_LS+1) + MIN_LS;
+
+    for (int i = 0; i < size; ++i)
+        list.push_back(rand() % (MAX_NR-MIN_NR+1) + MIN_NR);
+    cout << "List forward: ";
+    list.print();
+
+    cout << "List backward: ";
+    list.print_reverse();
+
+    cout << "Deleting list, then trying to print.\n";
+    list.~DoublyLinkedList();
+    cout << "List forward: ";
+    list.print();
+
+    return 0;
+}
