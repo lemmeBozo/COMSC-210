32ebbe76bad1940593c01e0844d38ef256e7473a
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-10-27 20:22:36 -0700

    10 minute auto commit

--- Lab 25 Data Structures Race/main.cpp
+++ Lab 25 Data Structures Race/main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iomanip>
 #include <algorithm> // for std::sort (that way I can use the same sort method? function on all the Data structures)
+#include <iterator> // for std::next
 
 #include <vector>
 #include <list>
@@ -25,6 +26,21 @@ auto timeFunction(function func, T& arg) {
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(end - start);
     return duration.count(); // will return elapsed time in milliseconds;
+}
+
+
+// function template to test out functions
+template <typename function, typename T>
+auto timeFunctionNS(function func, T& arg) {
+    auto start = high_resolution_clock::now();
+
+    // Calling the passed function with its provided arguments (if any)
+    // going to assume there is no return statement
+    func(arg);
+
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<nanoseconds>(end - start);
+    return duration.count(); // will return elapsed time in nanoseconds;
 }
 
 // Reading functions prototypes
@@ -61,6 +77,11 @@ int main() {
         << setw(10) << timeFunction(sortVector, vectorList) << "ms"
         << setw(10) << timeFunction(sortList, listList) << "ms"
         << setw(10) << "-1ms" << endl;
+
+        cout << setw(12) << "Insert"
+        << setw(10) << timeFunctionNS(insertVector, vectorList) << "ns"
+        << setw(10) << timeFunctionNS(insertList, listList) << "ns"
+        << setw(10) << timeFunctionNS(insertSet, setList) << "ns" << endl;
 
     return 0;
 }
@@ -100,7 +121,7 @@ void setRead(set<string> &l) {
     ifstream file("codes.txt"); // opening file
 
     if (!file) {
-        cout << "Unable to open file!" << endl;but 
+        cout << "Unable to open file!" << endl;
         return;
     }
     string line;
@@ -128,3 +149,17 @@ void insertVector(vector<string> &l) {
     int middle = l.size() / 2;
     l.at(middle) = "TESTCODE";
 }
+
+
+void insertList(list<string> &l) {
+    // The below is going to calculate the midpoint
+    // this is done by advancing the iterator to the middle spot of the list
+    auto middle = next(l.begin(), l.size() / 2); 
+
+    // Inserting at the middle
+    l.insert(middle, "TESTCODE");
+}
+
+void insertSet(set<string> &l) {
+    l.insert("TESTCODE");
+} 
