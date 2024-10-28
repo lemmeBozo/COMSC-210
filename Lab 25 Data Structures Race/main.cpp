a2e69c27114a40418ae9d4328fc79f281878e612
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-10-27 19:12:48 -0700

    Starting assignment

---
+++ Lab 25 Data Structures Race/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// COMSC-210 | Lab 25: Data Structures Races | Erick Pascual-Bautista
+
+int main() {
+    
+    return 0;
+}
+
+/* syntax examples:
+auto start = high_resolution_clock::now()
+auto end = high_resolution_clock::now()
+auto duration = duration_cast<milliseconds>(end - start)
+duration.count() references elapsed milliseconds
+*/
