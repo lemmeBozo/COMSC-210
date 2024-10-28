bbc3e936ba9877dd222bfb447ad26fac18312c26
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-10-27 19:33:01 -0700

    10 minute auto commit

--- Lab 25 Data Structures Race/main.cpp
+++ Lab 25 Data Structures Race/main.cpp
@@ -1,35 +1,49 @@
 #include <iostream>
 #include <chrono>
+#include <fstream>
+
+#include <vector>
+
 using namespace std;
 using namespace std::chrono;
 
 // COMSC-210 | Lab 25: Data Structures Races | Erick Pascual-Bautista
 
-// Use function template for timer?
-// and you just pass in the string somehow to call the appropriate funciton
+// function template to test out functions
 template <typename function, typename... Args>
-auto timeFunction(function name, Args... args) {
+auto timeFunction(function func, Args... args) {
     auto start = high_resolution_clock::now();
 
     // Calling the passed function with its provided arguments (if any)
-    auto result = func(args...);
+    // going to assume there is no return statement
+    func(args...);
 
     auto end = high_resolution_clock::now();
 
     auto duration = duration_cast<milliseconds>(end - start);
+    return duration;
 }
 
+
+// functions to test
+void vectorRead() {
+    vector<string> list;
+
+    ifstream file("codes.txt"); // opening the file
+
+    if(!file) {
+        cout << "Unable to open file!" << endl;
+        return;
+    }
+    string line; 
+    while (getline(file , line)) {
+        list.push_back(line); // adding each line to the vector
+    }
+    file.close(); // close the file after reading
+} 
+
 int main() {
-    // Start timing
-    auto start  = high_resolution_clock::now();
-
-
-    // Test 1 Reading 20,000 values
-    // put code here
-
-    auto end = high_resolution_clock::now();
-
-    auto duration = duration_cast<milliseconds>(end - start);
+    cout << "Read Vector operation: " << timeFunction(vectorRead) << endl;
 
     return 0;
 }
@@ -40,3 +54,4 @@ auto end = high_resolution_clock::now()
 auto duration = duration_cast<milliseconds>(end - start)
 duration.count() references elapsed milliseconds
 */
+
