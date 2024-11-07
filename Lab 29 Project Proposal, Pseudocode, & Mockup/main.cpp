68b28929ec83344bcc56440de2a52efb4ae7d19e
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-11-06 19:42:12 -0800

    10 minute auto commit

--- Lab 29 Project Proposal, Pseudocode, & Mockup/main.cpp
+++ Lab 29 Project Proposal, Pseudocode, & Mockup/main.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+#include <string>
 
 using namespace std;
 
 // COMSC-210 | Lab 29: Project Proposal, Pseudocode, & Mockup | Erick Pascual-Bautista
 
+// function prototypes
+void simulateTrafficFlow(map<string, vector<list<string>>>&);
+
+
 int main() {
 
     return 0;
+}
+
+// Simulates traffic light changes and manages car states
+
+//the string is the key, while the data is a vector of lists of strings
+void simulateTrafficFlow(map<string, vector<list<string>>>& lanes) {
+    // Pseudo code: 
+    // 1. Iterate throughout each lane in the map
+    // 2. Depending on the time, change the light state (i.e. green, red, yellow)
+    // 3. Adjust the car lists based on the light state
+    // 4. Print or log the traffic changes for each lane at this itme periond
 }
 
