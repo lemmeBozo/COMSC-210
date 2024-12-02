c8e48116b4a9d41366a218dff5ed247164c4a423
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-12-01 17:08:22 -0800

    10 minute auto commit

--- Lab 36 Records BST/main.cpp
+++ Lab 36 Records BST/main.cpp
@@ -4,23 +4,65 @@ using namespace std;
 
 // COMSC-210 | Lab 36: Records BST | Erick Pascual-Bautista
 
+// Global Variable
+int QUIT_CONDITION = 5;
+
 // Function Prototypes
-void printMenu(); // REUSING CODE from Lab 27
+void printMenu(); 
+void printLineBreaks(int);
+int getUserInput();
+void handleUserInput(int);
 
 int main() {
-    // TODO
-    // 1. Write a function that extracts a line at random from the file (returns string)
-    // 2. Write Interactive menu
-    
+    while (true) {
+        printMenu();
+        int userInput = getUserInput();
+        if (userInput == QUIT_CONDITION) {
+            cout << "Exiting program..." << endl;
+            break; 
+        }
+
+        printLineBreaks(2);
+    }
     return 0;
 }
 
 // displays the menu
-void printMenu() { // REUSING CODE from lab 27
+void printMenu() { 
         cout << "*** Binary Search Tree Operation  ***" << endl;
         cout << "[1] Add element" << endl;
         cout << "[2] Delete element" << endl;
         cout << "[3] Search for element" << endl;
         cout << "[4] Modify records at" << endl;
+        cout << "[5] QUIT" << endl;
         cout << "ENTER CHOICE: ";
+}
+
+void printLineBreaks(int breaks) {
+    for (int i = 0; i < breaks; i++) {
+        cout << endl;
+    }
+}
+
+int getUserInput() {
+    int value;
+    cin >> value;
+    return value;
+}
+
+void handleUserInput(int userInput) {
+    switch(userInput) {
+        case 1:
+            // Call addElement()
+            break;
+        case 2:
+            // Call deleteElement()
+            break;
+        case 3:
+
+        case 4:
+        case 5:
+        default:
+            break;
+    }
 }
