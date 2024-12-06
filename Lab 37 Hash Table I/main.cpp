7b4b823ade64e86156373291b252d56f707e049d
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-12-05 17:13:05 -0800

    PART 3 COMPLETE (FINISHED WORK)

--- Lab 37 Hash Table I/main.cpp
+++ Lab 37 Hash Table I/main.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 // Function prototypes
-int toAscii(string);
+int gen_hash_index(string);
 
 int main() {
     // Creatnig Hash Table (map of key ints, value list of strings)
@@ -17,10 +17,10 @@ int main() {
 
     string line;
     while(getline(file , line)) {
-        hash_table[toAscii(line)].push_back(line);
+        hash_table[gen_hash_index(line)].push_back(line);
     }
 
-    // Outputing 10 elements from the map
+    // Outputing elementsToOutput number of elements from the map
     int count = 0;
     int elementsToOutput = 30;
     for(auto iterator = hash_table.begin(); iterator != hash_table.end() && count < elementsToOutput; iterator++, count++) {
@@ -35,18 +35,10 @@ int main() {
     return 0;
 }
 
-int toAscii(string str) {
+int gen_hash_index(string str) {
     int sum = 0;
     for (auto& character : str) {
         sum += int (character);
     }
     return sum;
 }
-
-/* 
-These targets are present in the dataset and can be used for testing:
-536B9DFC93AF
-1DA9D64D02A0
-666D109AA22E
-E1D2665B21EA
-*/
